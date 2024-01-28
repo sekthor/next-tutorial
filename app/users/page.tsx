cc97b58316c1d0f667e000742396bd7ae9e2ba2d
sekthor <sekthor@protonmail.ch>
2024-01-28 20:59:51 +0100

    feat: use daisyui

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 interface User {
     id: number;
     name: string;
+    email: string;
 }
 
 const UsersPage = async () => {
@@ -20,9 +21,20 @@ const UsersPage = async () => {
   return (
     <>
         <h1>Users</h1>
-        <ul>
-            {users.map(user => <li key={user.id}>{user.name}</li>)}
-        </ul>
+        <table className='table table-bordered'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              </tr>)}
+          </tbody>
+        </table>
     </>
   )
 }

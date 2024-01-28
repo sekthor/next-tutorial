0d3704ada417c66d1101c618fa2a849a23248d84
sekthor <sekthor@protonmail.ch>
2024-01-28 20:38:15 +0100

    feat: styling and reorganisation of components

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import Image from 'next/image'
 import Link from 'next/link'
-import ProductCard from './components/ProductCard'
+import ProductCard from './components/ProductCard/ProductCard'
 
 export default function Home() {
   return (
